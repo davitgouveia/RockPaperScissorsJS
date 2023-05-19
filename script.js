3624ef581cfc5636b0fdde14ad0d38a12fcdb3dc
Davi Gouveia <davitgouveia@gmail.com>
2023-05-19 08:39:23 -0300

    result fix

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+const choices = ["✊", "🖐", "✌"];
+
 function game(playerChoice) {
-    const cpuChoice = cpuPlay()
+    var cpuChoice = cpuPlay()
+    document.getElementById("cpuChoice").textContent = `${cpuChoice}`;
+
     if (cpuChoice === playerChoice) {
         document.getElementById("gameResult").innerHTML = "<span style='font-size:2em'>TIE!</span>";
     } else if (
-        playerChoice === choices[0] && cpuChoice[2] ||
-        playerChoice === choices[1] && cpuChoice[0] ||
-        playerChoice === choices[2] && cpuChoice[1]
+        playerChoice === choices[0] && cpuChoice === choices[2]||
+        playerChoice === choices[1] && cpuChoice === choices[0] ||
+        playerChoice === choices[2] && cpuChoice === choices[1]
     ) {
         document.getElementById("gameResult").innerHTML = "<span style='color:green;font-size:2em'>YOU WIN!</span>";
     } else {
         document.getElementById("gameResult").innerHTML = "<span style='color:red;font-size:2em'>YOU LOST!</span>";
     }
+
     var change_path = function(){window.location.href="game.html"};
     setTimeout(change_path ,3000);
 }
 
-const choices = ["✊", "🖐", "✌"];
 
 /*CPU Play*/
 function cpuPlay() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     const result = choices[randomIndex];
-    document.getElementById("cpuChoice").textContent = `${result}`;
     return result;
 }
 
